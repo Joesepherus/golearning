c30b657243e48f67178facf77ceb97c6959c2bc4
Joseph Maloch <jozefmaloch@gmail.com>
2024-09-05 18:53:45 +0800

    Add integers and test for it.

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"golearning/helloworld"
+	"golearning/iteration"
+	"log"
 )
 
 func main() {
 	fmt.Println(helloworld.Hello("Chris", ""))
+	repeated := iteration.Repeat("k")
+	log.Print("repeated: ", repeated)
 }
